fc3fbeca0bba284ba2760e2b97e5bd8118de7418
kali19 <khadijah.syed.ali@gmail.com>
2023-08-08 18:25:22 -0400

    projects and footer formatted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,21 @@ import '../styles/Footer.css';
 
 function Footer() {
   return (
-    <footer className="footer">
-    <h3 id="contact">Contact</h3>
-    <ul id="list">
-      <li id="github"><a href="https://github.com/kali20987">GitHub</a></li>
-      <li id="linkedin"><a href="https://www.linkedin.com/in/khadijah-ali-42ba3b274/">LinkedIn</a></li>
-      <li id="resume"><a href="">Resume</a></li>
-      </ul>
-    </footer>
+    <div>
+      <footer className="footer">
+        <h3 id="contact">Contact</h3>
+        {/* <ul id="list"> */}
+      </footer>
+      <div class="footerrow">
+        {/* <li id="github" class="footercolumn"><a href="https://github.com/kali20987">GitHub</a></li>
+        <li id="linkedin" class="footercolumn"><a href="https://www.linkedin.com/in/khadijah-ali-42ba3b274/">LinkedIn</a></li>
+        <li id="resume" class="footercolumn"><a href="">Resume</a></li> */}
+        <div id="github" class="footercolumn"><a href="https://github.com/kali20987">GitHub</a></div>
+        <div id="linkedin" class="footercolumn"><a href="https://www.linkedin.com/in/khadijah-ali-42ba3b274/">LinkedIn</a></div>
+        <div id="resume" class="footercolumn"><a href="/images/KhadijahResume.pdf" download>Resume</a></div>
+        {/* </ul> */}
+      </div>
+    </div>
   );
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,60 +3,68 @@ import weight from '../images/weight.jpg';
 import job from '../images/job.jpg';
 
 function Projects() {
-    return (
-        <div>
-            <h1 id="projectsHeader">Projects</h1>
-           
-          <div class="card col-10 mx-auto">
-              <div class="row justify-content-md-center">
-                  <div id="filmimage"class="col-8 aboutBody">
-                    <a href="https://github.com/kali20987/FilmFetch">
-                  <img src={film} class="img-fluid rounded-start" alt="film icon"/>
-                  </a>
-                    <div id="filmBody">
-                        <h3>FilmFetch</h3>
-                        <p> A movie search website that I and three other developers created. It allows users to search movies by genre. 
-                        It uses HTML, CSS, JavaScript, themoviedb API, and the movieglu API.
-                        </p>
-                    </div>
-                  </div>
-              </div>
+  return (
+    <div>
+      <h1 id="projectsHeader">Projects</h1>
+      <div class="card col-10 mx-auto">
+        <div class="row justify-content-md-center">
+
+          <div id="filmimage" class="column aboutBody">
+            <a href="https://github.com/kali20987/FilmFetch">
+              <img src={film} class="img-fluid rounded-start" alt="film icon" />
+            </a>
           </div>
 
-          <div class="card col-10 mx-auto">
-              <div class="row justify-content-md-center">
-                  <div id="workoutimage"class="col-8 aboutBody">
-                    <a href="https://github.com/kali20987/Workout-Like-A-Boss">
-                  <img src={weight} class="img-fluid rounded-start" alt="weight icon"/>
-                  </a>
-                    <div id="workoutbox">
-                        <h3>Workout-Like-A-Boss</h3>
-                        <p> A workout suggestion website I and four other developers created which allows users to receive a workout based on what muscle group they would like to work on. 
-                        It uses Handlebars.js, Bootstrap, JavaScript, Node.js, and SQL.
-                        </p>
-                    </div>
-                  </div>
-              </div>
+          <div class="column">
+            <div id="filmBody">
+              <h3>FilmFetch</h3>
+              <p> A movie search website that I and three other developers created. It allows users to search movies by genre.
+                It uses HTML, CSS, JavaScript, themoviedb API, and the movieglu API.
+              </p>
+            </div>
           </div>
 
-          <div class="card col-10 mx-auto">
-              <div class="row justify-content-md-center">
-                  <div id="jobimage"class="col-8 aboutBody">
-                    <a href="https://github.com/kali20987/JobCanvas">
-                  <img src={job} class="img-fluid rounded-start" alt="job icon"/>
-                  </a>
-                    <div id="jobBox">
-                        <h3>JobCanvas</h3>
-                        <p> A job-searching website I and four other developers created which allows users to search and post jobs.
-                        It uses HTML, CSS, Bootstrap, JavaScript, MERN, and the Adzuna API.
-                        </p>
-                    </div>
-                  </div>
-              </div>
+        </div>
+      </div>
+
+      <div class="card col-10 mx-auto">
+        <div class="row justify-content-md-center">
+
+          <div class="column">
+            <div id="workoutbox">
+              <h3>Workout-Like-A-Boss</h3>
+              <p> A workout suggestion website I and four other developers created which allows users to receive a workout based on what muscle group they would like to work on.
+                It uses Handlebars.js, Bootstrap, JavaScript, Node.js, and SQL.
+              </p>
+            </div>
           </div>
 
-      </div>     
-    )
+          <div id="workoutimage" class="aboutBody column" text-align= "center">
+            <a href="https://github.com/kali20987/Workout-Like-A-Boss" text-align= "center">
+              <img src={weight} class="img-fluid rounded-start" alt="weight icon" height= "55%" width="55%" text-align= "center"/>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card col-10 mx-auto">
+        <div class="row justify-content-md-center">
+          <div id="jobimage" class="col-8 aboutBody">
+            <a href="https://github.com/kali20987/JobCanvas">
+              <img src={job} class="img-fluid rounded-start" alt="job icon" />
+            </a>
+            <div id="jobBox">
+              <h3>JobCanvas</h3>
+              <p> A job-searching website I and four other developers created which allows users to search and post jobs.
+                It uses HTML, CSS, Bootstrap, JavaScript, MERN, and the Adzuna API.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default Projects;
